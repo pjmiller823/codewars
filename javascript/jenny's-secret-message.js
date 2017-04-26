0504f122dd10cbbdfb1dc6054bf1db89bdb7e1e1
Patrick Miller <pjmiller823@gmail.com>
2017-04-26 16:35:44 -0400

    added and completed two js katas

---
+++ javascript/jenny's-secret-message.js
@@ -0,0 +1,7 @@
+https://www.codewars.com/kata/jennys-secret-message/train/javascript
+
+function greet(name){
+  if(name === "Johnny")
+    return "Hello, my love!";
+  return "Hello, " + name + "!";
+}
---
+++ javascript/multiply.js
@@ -0,0 +1,6 @@
+https://www.codewars.com/kata/50654ddff44f800200000004/solutions/javascript
+
+
+function multiply(a, b){
+ return a * b
+}
